8ed6501553c76c555a38cdb30c84f33624a15b98
Baseer Noorzai <121725504+Basnoo05@users.noreply.github.com>
2025-05-15 20:16:29 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'StudBudz'
+copyright = '2025, StudBudz'
+author = 'Buddies'
 
 release = '0.1'
 version = '0.1.0'
